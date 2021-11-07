e4af2b2b199c65631e10d41ba2b47c11e99821d2
tuna <tunahan.unsal@ceng.deu.edu.tr>
2021-11-07 23:21:56 +0300

    first commit <3

--- app/src/main/java/com/senior/businesscard/view/activities/MainActivity.java
+++ app/src/main/java/com/senior/businesscard/view/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.senior.businesscard.view.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.senior.businesscard.R;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG", "onCreate: asd");
     }
 }
